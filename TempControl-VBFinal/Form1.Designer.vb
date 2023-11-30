84099c36688d699a3291138bac19fd49bd3b1665
bbre1666 <badenbrenner@isu.edu>
2023-11-30 09:06:59 -0700

    Adding Qy@ -analog works digital still needs to get setup

--- TempControl-VBFinal/Form1.Designer.vb
+++ TempControl-VBFinal/Form1.Designer.vb
@@ -32,17 +32,29 @@ Partial Class Form1
         Me.BaudRateToolStripComboBox = New System.Windows.Forms.ToolStripComboBox()
         Me.ComPortToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ComPortToolStripComboBox = New System.Windows.Forms.ToolStripComboBox()
+        Me.SaveSettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ConnectButton = New System.Windows.Forms.Button()
         Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
-        Me.ExitButton = New System.Windows.Forms.Button()
-        Me.HighSetPointTextBox = New System.Windows.Forms.TextBox()
-        Me.LowSetPointTextBox = New System.Windows.Forms.TextBox()
+        Me.CoolingSetPointTextBox = New System.Windows.Forms.TextBox()
+        Me.HeatingSetPointTextBox = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.SaveSettingsButton = New System.Windows.Forms.Button()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.TimeLabel = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.UPCoolingButtion = New System.Windows.Forms.Button()
+        Me.UPHeatButton = New System.Windows.Forms.Button()
+        Me.DownCoolingButton = New System.Windows.Forms.Button()
+        Me.DownHeatButton = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
@@ -73,7 +85,7 @@ Partial Class Form1
         '
         'SettingsToolStripMenuItem
         '
-        Me.SettingsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SerialPortToolStripMenuItem})
+        Me.SettingsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SerialPortToolStripMenuItem, Me.SaveSettingsToolStripMenuItem, Me.ExitToolStripMenuItem})
         Me.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
         Me.SettingsToolStripMenuItem.ShowShortcutKeys = False
         Me.SettingsToolStripMenuItem.Size = New System.Drawing.Size(76, 24)
@@ -83,7 +95,7 @@ Partial Class Form1
         '
         Me.SerialPortToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.BaudRateToolStripMenuItem, Me.ComPortToolStripMenuItem})
         Me.SerialPortToolStripMenuItem.Name = "SerialPortToolStripMenuItem"
-        Me.SerialPortToolStripMenuItem.Size = New System.Drawing.Size(159, 26)
+        Me.SerialPortToolStripMenuItem.Size = New System.Drawing.Size(180, 26)
         Me.SerialPortToolStripMenuItem.Text = "Serial Port"
         '
         'BaudRateToolStripMenuItem
@@ -95,6 +107,7 @@ Partial Class Form1
         '
         'BaudRateToolStripComboBox
         '
+        Me.BaudRateToolStripComboBox.Font = New System.Drawing.Font("Segoe UI", 9.0!)
         Me.BaudRateToolStripComboBox.Name = "BaudRateToolStripComboBox"
         Me.BaudRateToolStripComboBox.Size = New System.Drawing.Size(121, 28)
         '
@@ -107,8 +120,21 @@ Partial Class Form1
         '
         'ComPortToolStripComboBox
         '
+        Me.ComPortToolStripComboBox.Font = New System.Drawing.Font("Segoe UI", 9.0!)
         Me.ComPortToolStripComboBox.Name = "ComPortToolStripComboBox"
         Me.ComPortToolStripComboBox.Size = New System.Drawing.Size(121, 28)
+        '
+        'SaveSettingsToolStripMenuItem
+        '
+        Me.SaveSettingsToolStripMenuItem.Name = "SaveSettingsToolStripMenuItem"
+        Me.SaveSettingsToolStripMenuItem.Size = New System.Drawing.Size(180, 26)
+        Me.SaveSettingsToolStripMenuItem.Text = "Save Settings"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(180, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'ConnectButton
         '
@@ -119,67 +145,131 @@ Partial Class Form1
         Me.ConnectButton.Text = "Connect"
         Me.ConnectButton.UseVisualStyleBackColor = True
         '
-        'ExitButton
+        'SerialPort1
         '
-        Me.ExitButton.Location = New System.Drawing.Point(12, 406)
-        Me.ExitButton.Name = "ExitButton"
-        Me.ExitButton.Size = New System.Drawing.Size(64, 32)
-        Me.ExitButton.TabIndex = 4
-        Me.ExitButton.Text = "Exit"
-        Me.ExitButton.UseVisualStyleBackColor = True
         '
-        'HighSetPointTextBox
+        'CoolingSetPointTextBox
         '
-        Me.HighSetPointTextBox.Location = New System.Drawing.Point(732, 55)
-        Me.HighSetPointTextBox.Name = "HighSetPointTextBox"
-        Me.HighSetPointTextBox.Size = New System.Drawing.Size(56, 22)
-        Me.HighSetPointTextBox.TabIndex = 5
+        Me.CoolingSetPointTextBox.Location = New System.Drawing.Point(673, 55)
+        Me.CoolingSetPointTextBox.Name = "CoolingSetPointTextBox"
+        Me.CoolingSetPointTextBox.Size = New System.Drawing.Size(56, 22)
+        Me.CoolingSetPointTextBox.TabIndex = 5
         '
-        'LowSetPointTextBox
+        'HeatingSetPointTextBox
         '
-        Me.LowSetPointTextBox.Location = New System.Drawing.Point(732, 83)
-        Me.LowSetPointTextBox.Name = "LowSetPointTextBox"
-        Me.LowSetPointTextBox.Size = New System.Drawing.Size(56, 22)
-        Me.LowSetPointTextBox.TabIndex = 6
+        Me.HeatingSetPointTextBox.Location = New System.Drawing.Point(673, 157)
+        Me.HeatingSetPointTextBox.Name = "HeatingSetPointTextBox"
+        Me.HeatingSetPointTextBox.Size = New System.Drawing.Size(56, 22)
+        Me.HeatingSetPointTextBox.TabIndex = 6
         '
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(629, 58)
+        Me.Label1.Location = New System.Drawing.Point(559, 58)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(87, 16)
+        Me.Label1.Size = New System.Drawing.Size(108, 16)
         Me.Label1.TabIndex = 7
-        Me.Label1.Text = "High Setpoint"
+        Me.Label1.Text = "Clooling Setpoint"
         '
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(629, 86)
+        Me.Label2.Location = New System.Drawing.Point(559, 160)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(83, 16)
+        Me.Label2.Size = New System.Drawing.Size(106, 16)
         Me.Label2.TabIndex = 8
-        Me.Label2.Text = "Low Setpoint"
+        Me.Label2.Text = "Heating Setpoint"
         '
-        'SaveSettingsButton
+        'PictureBox3
         '
-        Me.SaveSettingsButton.Location = New System.Drawing.Point(687, 368)
-        Me.SaveSettingsButton.Name = "SaveSettingsButton"
-        Me.SaveSettingsButton.Size = New System.Drawing.Size(101, 32)
-        Me.SaveSettingsButton.TabIndex = 9
-        Me.SaveSettingsButton.Text = "Save Settings"
-        Me.SaveSettingsButton.UseVisualStyleBackColor = True
+        Me.PictureBox3.Location = New System.Drawing.Point(135, 79)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox3.TabIndex = 10
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Location = New System.Drawing.Point(135, 157)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(100, 50)
+        Me.PictureBox4.TabIndex = 11
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Location = New System.Drawing.Point(282, 109)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(73, 57)
+        Me.PictureBox5.TabIndex = 12
+        Me.PictureBox5.TabStop = False
+        '
+        'TimeLabel
+        '
+        Me.TimeLabel.AutoSize = True
+        Me.TimeLabel.Location = New System.Drawing.Point(427, 55)
+        Me.TimeLabel.Name = "TimeLabel"
+        Me.TimeLabel.Size = New System.Drawing.Size(38, 16)
+        Me.TimeLabel.TabIndex = 13
+        Me.TimeLabel.Text = "Time"
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 250
+        '
+        'UPCoolingButtion
+        '
+        Me.UPCoolingButtion.Location = New System.Drawing.Point(735, 31)
+        Me.UPCoolingButtion.Name = "UPCoolingButtion"
+        Me.UPCoolingButtion.Size = New System.Drawing.Size(53, 24)
+        Me.UPCoolingButtion.TabIndex = 14
+        Me.UPCoolingButtion.Text = "UP"
+        Me.UPCoolingButtion.UseVisualStyleBackColor = True
+        '
+        'UPHeatButton
+        '
+        Me.UPHeatButton.Location = New System.Drawing.Point(735, 142)
+        Me.UPHeatButton.Name = "UPHeatButton"
+        Me.UPHeatButton.Size = New System.Drawing.Size(53, 24)
+        Me.UPHeatButton.TabIndex = 15
+        Me.UPHeatButton.Text = "UP"
+        Me.UPHeatButton.UseVisualStyleBackColor = True
+        '
+        'DownCoolingButton
+        '
+        Me.DownCoolingButton.Location = New System.Drawing.Point(735, 70)
+        Me.DownCoolingButton.Name = "DownCoolingButton"
+        Me.DownCoolingButton.Size = New System.Drawing.Size(53, 24)
+        Me.DownCoolingButton.TabIndex = 16
+        Me.DownCoolingButton.Text = "Down"
+        Me.DownCoolingButton.UseVisualStyleBackColor = True
+        '
+        'DownHeatButton
+        '
+        Me.DownHeatButton.Location = New System.Drawing.Point(735, 172)
+        Me.DownHeatButton.Name = "DownHeatButton"
+        Me.DownHeatButton.Size = New System.Drawing.Size(53, 24)
+        Me.DownHeatButton.TabIndex = 17
+        Me.DownHeatButton.Text = "Down"
+        Me.DownHeatButton.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.SaveSettingsButton)
+        Me.Controls.Add(Me.DownHeatButton)
+        Me.Controls.Add(Me.DownCoolingButton)
+        Me.Controls.Add(Me.UPHeatButton)
+        Me.Controls.Add(Me.UPCoolingButtion)
+        Me.Controls.Add(Me.TimeLabel)
+        Me.Controls.Add(Me.PictureBox5)
+        Me.Controls.Add(Me.PictureBox4)
+        Me.Controls.Add(Me.PictureBox3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.LowSetPointTextBox)
-        Me.Controls.Add(Me.HighSetPointTextBox)
-        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.HeatingSetPointTextBox)
+        Me.Controls.Add(Me.CoolingSetPointTextBox)
         Me.Controls.Add(Me.ConnectButton)
         Me.Controls.Add(Me.PictureBox2)
         Me.Controls.Add(Me.PictureBox1)
@@ -191,6 +281,9 @@ Partial Class Form1
         CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -207,10 +300,19 @@ Partial Class Form1
     Friend WithEvents BaudRateToolStripComboBox As ToolStripComboBox
     Friend WithEvents ConnectButton As Button
     Friend WithEvents SerialPort1 As IO.Ports.SerialPort
-    Friend WithEvents ExitButton As Button
-    Friend WithEvents HighSetPointTextBox As TextBox
-    Friend WithEvents LowSetPointTextBox As TextBox
+    Friend WithEvents CoolingSetPointTextBox As TextBox
+    Friend WithEvents HeatingSetPointTextBox As TextBox
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents SaveSettingsButton As Button
+    Friend WithEvents PictureBox3 As PictureBox
+    Friend WithEvents PictureBox4 As PictureBox
+    Friend WithEvents PictureBox5 As PictureBox
+    Friend WithEvents TimeLabel As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents SaveSettingsToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UPCoolingButtion As Button
+    Friend WithEvents UPHeatButton As Button
+    Friend WithEvents DownCoolingButton As Button
+    Friend WithEvents DownHeatButton As Button
 End Class
--- TempControl-VBFinal/Form1.vb
+++ TempControl-VBFinal/Form1.vb
@@ -3,6 +3,22 @@ Imports System.IO.Ports
 Public Class Form1
     Public DateTemp, DrivePath, fileName, ComPortName, Baudrate, ComPortName1, Baudrate1, HighSetPoint, LowSetPoint As String
     Public PortState As Boolean
+    Public ReceveByte(18) As Byte
+    Public AnalogData1, AnalogData2, AnalogData3, AnalogData4, DigitalData As Integer
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        TimeLabel.Text = TimeOfDay
+
+
+        If PortState = True Then        'if serial port is open send read analog in comand 
+            Dim Intermidete() As Byte = {&H53} 'load comand to byte array
+            SerialPort1.Write(Intermidete, 0, Intermidete.Length)     'output comand 
+
+            Dim Intermidete1() As Byte = {&H30} 'load comand to byte array
+            SerialPort1.Write(Intermidete1, 0, Intermidete1.Length)     'output comand 
+        End If
+
+    End Sub
 
     Private Sub BaudRateToolStripComboBox_Click(sender As Object, e As EventArgs) Handles BaudRateToolStripComboBox.Click
         BaudRateToolStripComboBox.Items.Clear()        'Display Baud rate Options
@@ -17,6 +33,46 @@ Public Class Form1
         BaudRateToolStripComboBox.Items.Add(230400)
         BaudRateToolStripComboBox.Items.Add(460800)
         BaudRateToolStripComboBox.Items.Add(921600)
+    End Sub
+
+    Private Sub UPCoolingButtion_Click(sender As Object, e As EventArgs) Handles UPCoolingButtion.Click
+        CoolingSetPointTextBox.Text += 0.5
+    End Sub
+
+    Private Sub DownCoolingButton_Click(sender As Object, e As EventArgs) Handles DownCoolingButton.Click
+        CoolingSetPointTextBox.Text -= 0.5
+    End Sub
+
+    Private Sub UPHeatButton_Click(sender As Object, e As EventArgs) Handles UPHeatButton.Click
+        HeatingSetPointTextBox.Text += 0.5
+    End Sub
+
+    Private Sub DownHeatButton_Click(sender As Object, e As EventArgs) Handles DownHeatButton.Click
+        HeatingSetPointTextBox.Text -= 0.5
+    End Sub
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Me.Close()
+    End Sub
+
+    Private Sub SaveSettingsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveSettingsToolStripMenuItem.Click
+        DrivePath = CurDir()
+
+        fileName = DrivePath & "\" & "HVACSettings" & ".txt" 'set the file name 
+
+        Try
+            System.IO.File.Delete(fileName)
+            FileOpen(1, fileName, OpenMode.Append)  'open the file
+        Catch ex As Exception
+            MsgBox("Faiiled to open the settings file, Please goto the setup menu")
+            Exit Sub
+        End Try
+
+        WriteLine(1, Baudrate)      'Write baud rate to file 
+        WriteLine(1, ComPortName)   'Write comport to file 
+        WriteLine(1, CoolingSetPointTextBox.Text)
+        WriteLine(1, HeatingSetPointTextBox.Text)
+
+        FileClose(1)
     End Sub
 
     Private Sub ComPortToolStripComboBox_Click(sender As Object, e As EventArgs) Handles ComPortToolStripComboBox.Click
@@ -60,8 +116,8 @@ Public Class Form1
         End Try
         ConnectButton.Text = "Connect"    'set the button text back to connect 
         PortState = False   'set the port state indicator to indicat the port is closed
-        SerialPort1.BaudRate = Baudrate    'set the baud to the selected item in the list 
-        SerialPort1.PortName = ComPortName   'set the selected port name 
+        SerialPort1.BaudRate = Baudrate1    'set the baud to the selected item in the list 
+        SerialPort1.PortName = ComPortName1   'set the selected port name 
 
 
 
@@ -86,38 +142,30 @@ Public Class Form1
         End If
         FileClose(1)    'close file
     End Sub
-
-    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
-        Me.Close()
-    End Sub
-
-    Private Sub SaveSettingsButton_Click(sender As Object, e As EventArgs) Handles SaveSettingsButton.Click
-
-        DrivePath = CurDir()
-
-        fileName = DrivePath & "\" & "HVACSettings" & ".txt" 'set the file name 
-
-        Try
-            System.IO.File.Delete(fileName)
-            FileOpen(1, fileName, OpenMode.Append)  'open the file
-        Catch ex As Exception
-            MsgBox("Faiiled to open the settings file, Please goto the setup menu")
-            Exit Sub
-        End Try
-
-        WriteLine(1, Baudrate)      'Write baud rate to file 
-        WriteLine(1, ComPortName)   'Write comport to file 
-        WriteLine(1, HighSetPointTextBox.Text)
-        WriteLine(1, LowSetPointTextBox.Text)
-
-        FileClose(1)
-    End Sub
-
     Private Sub BaudRateToolStripComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles BaudRateToolStripComboBox.SelectedIndexChanged
         Baudrate = CInt(BaudRateToolStripComboBox.SelectedItem)
     End Sub
 
     Private Sub ComPortToolStripComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComPortToolStripComboBox.SelectedIndexChanged
         ComPortName = ComPortToolStripComboBox.SelectedItem
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Timer1.Enabled = True
+        HeatingSetPointTextBox.Text = 65
+        CoolingSetPointTextBox.Text = 70
+
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As Object, e As SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim RaadSize = SerialPort1.BytesToRead
+
+        SerialPort1.Read(ReceveByte, 0, 4)
+        AnalogData1 = ReceveByte(0)         'store data for analog to the analog data vribles
+        AnalogData2 = ReceveByte(1)
+        AnalogData3 = ReceveByte(2)
+        AnalogData4 = ReceveByte(3)
+        DigitalData = ReceveByte(4)
+
     End Sub
 End Class
